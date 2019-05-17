b7f187cc4b27546a73bd4fe90c08c27256c58ed8
saltowl <vladislava-shakirova@mail.ru>
2019-05-17 18:02:18 +0300

    Add generating data for both functions

---
+++ NeuralNetwork/data_handling.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+def generate_data_for_first_func():
+	x = [0, 0, 1, 1]
+	y = [0, 1, 0, 1]
+
+	for i in range(5):
+		x.extend(x)
+		y.extend(y)
+
+	output = [int(not(x[i] or y[i]) or not(y[i]) or (x[i] and y[i])) for i in range(len(x))]
+
+	df = pd.DataFrame({'x':x, 'y':y, 'F':output})
+	np.random.shuffle(df.values)
+	df.to_csv('../data/data1.csv', index=False)
+
+def generate_data_for_second_func():
+	x = [0, 0, 0, 0, 1, 1, 1, 1]
+	y = [0, 0, 1, 1, 0, 0, 1, 1]
+	z = [0, 1, 0, 1, 0, 1, 0, 1]
+
+	for i in range(5):
+		x.extend(x)
+		y.extend(y)
+		z.extend(z)
+	
+	output = [int((not x[i] or not y[i]) and (not x[i] or z[i])) for i in range(len(x))]
+
+	df = pd.DataFrame({'x':x, 'y':y, 'z':z, 'F':output})
+	np.random.shuffle(df.values)
+	df.to_csv('../data/data2.csv', index=False)
